9073b4f0dc71f9eea5256277b1b17494a9a1c64c
AMANKUMAR1020 <92974468+AMANKUMAR1020@users.noreply.github.com>
2023-08-16 22:24:51 +0530

    Update Public.js
    admin added

--- src/components/Public.js
+++ src/components/Public.js
@@ -5,16 +5,16 @@ const Public = () => {
         <section className="public">
             <main><h1 className='public_headline'>Organize, Collaborate, and Empower Your Workflow with MyNote!</h1>
             <p className='public_detail-line'>Streamline your note-taking process with NoteCategorize, the ultimate note-making website that allows you to effortlessly categorize and manage your notes based on your role - admin, manager, or employee. Stay organized, collaborate seamlessly, and boost productivity with NoteCategorize!</p>
-            </main>
-          <div class="container_">
-    <h2 class="title_">We are providing some username and password</h2>
-    <p class="description_"> username-aman password-aman<br/>
-    username-admin password-admin<br/>
-    username-manager password-manager<br/>
-    username-employee password-employee<br/>
-    </p>
-    for more information go to <button class="button_"><a href="https://github.com/AMANKUMAR1020">Github page</a></button>
-  </div>
+            <div>
+            <h2 >We are providing some username and password</h2>
+            <p > username-aman password-aman<br/>
+            username-admin password-admin<br/>
+            username-manager password-manager<br/>
+            username-employee password-employee<br/>
+            </p>
+            for more information go to <button ><a href="https://github.com/AMANKUMAR1020">Github page</a></button>
+          </div>    
+        </main>
             <footer className='employee-login'>
                 <Link to="/login" >
                     Employee Login
