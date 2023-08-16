bf4a69586e3c50c23d483b55a467af8b6133eb76
AMANKUMAR1020 <92974468+AMANKUMAR1020@users.noreply.github.com>
2023-08-16 22:20:24 +0530

    Update Public.js
    admin added

--- src/components/Public.js
+++ src/components/Public.js
@@ -6,6 +6,15 @@ const Public = () => {
             <main><h1 className='public_headline'>Organize, Collaborate, and Empower Your Workflow with MyNote!</h1>
             <p className='public_detail-line'>Streamline your note-taking process with NoteCategorize, the ultimate note-making website that allows you to effortlessly categorize and manage your notes based on your role - admin, manager, or employee. Stay organized, collaborate seamlessly, and boost productivity with NoteCategorize!</p>
             </main>
+          <div class="container_">
+    <h2 class="title_">We are providing some username and password</h2>
+    <p class="description_"> username-aman password-aman<br/>
+    username-admin password-admin<br/>
+    username-manager password-manager<br/>
+    username-employee password-employee<br/>
+    </p>
+    for more information go to <button class="button_"><a href="https://github.com/AMANKUMAR1020">Github page</a></button>
+  </div>
             <footer className='employee-login'>
                 <Link to="/login" >
                     Employee Login
